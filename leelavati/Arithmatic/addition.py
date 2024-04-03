2441d25f6b35ff4ec1399a9e838e4febdb77c45e
suryghosh <ghoshsng16@gmail.com>
2024-04-03 17:15:39 +0530

    code for addition,subtraction,QE and unitconv

---
+++ leelavati/Arithmatic/addition.py
@@ -0,0 +1,13 @@
+def add(*args):
+    max_length = max(len(str(num)) for num in args)
+    sum_columns = [[int(str(num)[::-1][i]) if len(str(num)) > i else 0 for num in args] for i in range(max_length)]
+    column_sums = [sum(column) for column in sum_columns]       #[18,15,12]
+
+    res = 0
+    for i in range(len(column_sums)):
+        res += column_sums[i]10*(i)
+        
+    return res
+# Example usage:
+result = add(123, 456, 789)
+print(result)  # Output: [9+6+3, 8+5+2, 7+4+1]
